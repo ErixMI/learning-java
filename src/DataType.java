fb0a9834810520dbf1d5dedf24dac3e77ba3cd9e
erikm <erikstudyqa@gmail.com>
2024-03-04 20:43:42 +0200

    Not ready

---
+++ src/DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+    public static void main(String[] args) {
+        boolean isRaining = false;
+        boolean hasPassedExercise = true;
+        boolean isSummer = false;
+        System.out.println(isRaining);
+        System.out.println(hasPassedExercise);
+        System.out.println(isSummer);
+
+
+    }
+}
