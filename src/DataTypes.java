1223d70f5f6598ca963fe1d32ce190fadd6bb63f
erikm <erikstudyqa@gmail.com>
2024-03-11 14:39:42 +0200

    [feature/data_types]: Add DataTypes class

---
+++ src/DataTypes.java
@@ -0,0 +1,56 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        //First comment
+        /*
+        i am working on this file
+        Please do not hesitate to ask me
+        If you have a questions
+         */
+        // Variables : int
+
+        int numberOfStudents = 10;
+        int maxScore = 100;
+        System.out.println(numberOfStudents);
+
+        //Variables: double
+
+        double temperature = 3.4;
+        double price = 99.1;
+        System.out.println(temperature);
+
+        //Variables : float
+
+        float pi = 3.14f;
+        float radius = 5.5f;
+        float area = pi * radius * radius;
+        System.out.println(area);
+
+        // Casting
+        int areaInteger = (int) area;
+        System.out.println(areaInteger);
+
+        // Char
+        char sybmol = '$';
+        System.out.println(sybmol);
+        char n = 78;
+        System.out.println(n);
+
+        char e = 69;
+        System.out.println(e);
+        char r = 82;
+        System.out.println(r);
+        char i = 73;
+        System.out.println(i);
+        char k = 75;
+        System.out.println(k);
+
+        // Boolean is / has
+        boolean isRaining = false;
+        boolean hasPassedEcercise = true;
+        boolean isSummer = false;
+        System.out.println(isSummer);
+        System.out.println(isRaining);
+        System.out.println(hasPassedEcercise);
+    }
+}
